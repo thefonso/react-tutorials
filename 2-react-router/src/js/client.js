04e005c5a862d3ca507893e5d4b54846e4fafa39
thefonso <thefonso@thefonso.com>
2017-10-25 02:47:47 -0700

    2-react-router Bug-fix: font-awesome cdn link fixed

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -4,13 +4,18 @@ import {Router, Route, IndexRoute, hashHistory} from "react-router";
 
 
 import Layout from "./pages/Layout";
+import Featured from "./pages/Featured";
+import Archives from "./pages/Archives";
+import Settings from "./pages/Settings";
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Layout}>
-
+            <IndexRoute component={Featured}></IndexRoute>
+            <Route path="archives" component={Archives}></Route>
+            <Route path="settings" component={Settings}></Route>
         </Route>
     </Router>,
 app);
