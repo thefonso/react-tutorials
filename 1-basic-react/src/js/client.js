17e7c14b4add037a19ea3cda1bedbbd6c599f438
thefonso <thefonso@thefonso.com>
2017-10-24 18:56:32 -0700

    1-basic-react: example of invoking class methods

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
+    // example of invoking class methods 
+    getVal(){
+        return "fonso";
+    }
+
     render() {
-        // works like ruby
-        const name = "fonso";
         return(
-            <div>
-                <h1>It's Working</h1>
-                <h1>It's {name}</h1>
-                <h1>It's {3 * 2}</h1>
-                <h1>It's {(function () {return 3; })()}</h1>
-            </div>
+                <h1>It's {this.getVal()}!</h1>
         );
     }
 }
