6c7a88800fb6972da629f0d3b01dcd3012cf9259
thefonso <thefonso@thefonso.com>
2017-10-25 03:58:46 -0700

    2-react-router VIDEO 7: router params for archives path

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -13,9 +13,9 @@ const app = document.getElementById('app');
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Layout}>
-            <IndexRoute component={Featured}/>
-            <Route path="archives" component={Archives}/>
-            <Route path="settings" component={Settings}/>
+            <IndexRoute component={Featured}></IndexRoute>
+            <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+            <Route path="settings" name="settings" component={Settings}></Route>
         </Route>
     </Router>,
 app);
