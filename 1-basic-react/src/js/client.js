11d57b8f3c70f9504105eb4d7e1193d625dd874a
thefonso <thefonso@thefonso.com>
2017-10-24 18:51:33 -0700

    1-basic-react: examples showing how it works a bit like ruby

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
     render() {
+        // works like ruby
+        const name = "fonso";
         return(
-            <h1>It's Working!</h1>
+            <div>
+                <h1>It's Working</h1>
+                <h1>It's {name}</h1>
+                <h1>It's {3 * 2}</h1>
+                <h1>It's {(function () {return 3; })()}</h1>
+            </div>
         );
     }
 }
 
 const app = document.getElementById('app');
+
 ReactDOM.render(<Layout/>, app);
