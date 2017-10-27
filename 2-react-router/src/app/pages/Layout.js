685ee3f41b815a962fde54b86a8940f2a2d76379
thefonso <thefonso@thefonso.com>
2017-10-27 02:39:52 -0700

    2-react-router VIDEO 8: complete

--- 2-react-router/src/app/pages/Layout.js
+++ 2-react-router/src/app/pages/Layout.js
@@ -4,30 +4,30 @@ import Footer from "../components/layout/Footer";
 import Nav from "../components/layout/Nav";
 
 export default class Layout extends React.Component {
-    render() {
-        const { location } = this.props;
-        const containerStyle = {
-            marginTop: "60px"
-        };
-        console.log("layout");
-        return (
-            <div>
+  render() {
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
+    console.log("layout");
+    return (
+        <div>
 
-                <Nav location={location} />
+          <Nav location={location} />
 
-                <div class="container" style={containerStyle}>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h1>KillerNews.net</h1>
+          <div class="container" style={containerStyle}>
+            <div class="row">
+              <div class="col-lg-12">
+                <h1>KillerNews.net</h1>
 
-                            {this.props.children}
+                {this.props.children}
 
-                        </div>
-                    </div>
-                    <Footer/>
-                </div>
+              </div>
             </div>
+            <Footer/>
+          </div>
+        </div>
 
-        );
-    }
+    );
+  }
 }
