3e08553c7e1e26e06884d03f1d58a9454f9c68fa
thefonso <thefonso@thefonso.com>
2018-04-04 10:02:23 -0500

    added bootstrap native for no jquery

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Bootstrap from "../../node_modules/bootstrap.native/dist/bootstrap-native.js"
 import Layout from "./pages/Layout";
 
 const app = document.getElementById('app');
