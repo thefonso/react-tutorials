ae1dd011e918434210bf644c0608b8c0c61285b0
thefonso <thefonso@thefonso.com>
2018-04-04 11:14:24 -0500

    WIP end of video 6

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Bootstrap from "../../node_modules/bootstrap.native/dist/bootstrap-native.js"
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
+import Bootstrap from "../../node_modules/bootstrap.native/dist/bootstrap-native.js";
+
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
+import Settings from "./pages/Settings";
 import Layout from "./pages/Layout";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+            <IndexRoute component={Featured}></IndexRoute>
+            <Route path="archives" component={Archives}></Route>
+            <Route path="settings" component={Settings}></Route>
+        </Route>
+    </Router>,
+    app);
--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import {Link} from "react-router";
 
 
 export default class Layout extends React.Component {
+    navigate(){
+        this.props.history.pushState(null, "/")
+    }
     render() {
         return (
-            <h1>Killer News</h1>
+            <div>
+                <h1>KillerNews</h1>
+                {this.props.children}
+                <Link to="archives" class="btn btn-danger">archive</Link>
+                <Link to="settings"><button class="btn btn-success">settings</button></Link>
+                <button onClick={this.navigate.bind(this)}>featured</button>
+            </div>
+
         );
     }
 }
